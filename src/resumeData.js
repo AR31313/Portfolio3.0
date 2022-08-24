be99a531e992b5e11ce5bfc4b1b1cdd09d6d943d
AR31313 <gurlgotgame@gmail.com>
2022-08-24 12:15:41 -0700

    react components, data

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+  "imagebaseurl": "https://github.com/AR31313",
+  "name": "Avi Rana",
+  "role": "Designer and Developer",
+  "linkedinId": "https://www.linkedin.com/in/yay4avi/",
+  "skypeid": "https://yay4avi.wixsite.com/gisportfolio",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/yay4avi/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/AR31313",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "GIS",
+      "url": "https://yay4avi.wixsite.com/gisportfolio",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "Front-end Web Developer with background in Business Management, Environmental Science with extensive GIS Experience. Earned Certificate in Full Stack Web Development, GIS and Bachelor of Science Degree from University of Washington. Skills in Mongo, Express, React & Node; proficient with Microsoft Office, Esri & ArcGIS programs as well as Quickbooks. Known as a highly motivated problem solver with a strong commitment to continually learning new things and developing myself professionally.",
+  "address": "Greater Seattle Area",
+  "website": "yay4avi@uw.edu",
+  "education": [
+    {
+      "UniversityName": "Univeristy Of Washington",
+      "specialization": "Full Stack Web Development Certificate",
+      "MonthOfPassing": "August",
+      "YearOfPassing": "2022",
+      "Achievements": "Software Languages:  HTML, Bootstrap, CSS, JavScript & jQuery, node.js, MySQL, Express, Handlebars, React, APIs, Sequelize, GraphQL, Apollo"
+    },
+    {
+      "UniversityName": "Univeristy Of Washington",
+      "specialization": "Geographic Information Systems (GIS) Certificate",
+      "MonthOfPassing": "June",
+      "YearOfPassing": "2016",
+      "Achievements": "Softwares/Platforms: Esri’s ArcGIS, ArcMap, Quickbooks, Microsoft Office, Visual Studio Code, Git Bash, Insomnia, MongoDB, Adobe Suites"
+    },
+    {
+      "UniversityName": "Univeristy Of Washington",
+      "specialization": "Environmental Science",
+      "MonthOfPassing": "Jun",
+      "YearOfPassing": "2015",
+      "Achievements": "Minor in Sustainability "
+
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "RANAVI GIS SERVICES",
+      "specialization": "Oversee the use analytical skills necessary to apply GIS to show visualizations of common social and environmental problems. ",
+      "MonthOfLeaving": "",
+      "YearOfLeaving": "OWNER",
+      "Achievements": "Manage spatial information for Wildlife Management and create different tools for users to see data findings about wildlife."
+    },
+    {
+      "CompanyName": "Anu Rana's Healthy Kitchen",
+      "specialization": "Grew the company by 41% growth consistently every year and increased customer visibility.",
+      "MonthOfLeaving": "",
+      "YearOfLeaving": "MANAGER",
+      "Achievements": "Recognized by “Food & Wine Magazine” and other organizations as being an established place to visit in the community."
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "MONGO"
+    },
+    {
+      "skillname": "EXPRESS"
+    },
+    {
+      "skillname": "React"
+    },
+    {
+      "skillname": "Node.js"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
